99a4e7574c8ac073404924f2797cb3f465d59099
Keegan Manton <b9010662@hallam.shu.ac.uk>
2020-02-28 11:32:19 +0000

    Init Repo

---
+++ src/Ball.js
@@ -0,0 +1,34 @@
+export class Ball extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y, direction, collide = true) {
+    var texture = "playerBall";
+    super(scene, x, y, texture);
+
+    this.speed = 1500;
+    this.direction = direction;
+    this.collides = collide;
+
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+
+    this.move(this.direction);
+  }
+
+  move(direction) {
+    if (this.direction === "left") {
+      this.setVelocityX(-this.speed);
+    } else if (this.direction === "right") {
+      this.setVelocityX(this.speed);
+    } else if (this.direction === "up") {
+      this.setVelocityY(-this.speed);
+    } else if (this.direction === "down") {
+      this.setVelocityY(this.speed);
+    }
+  }
+
+  offScreen() {
+    if (this.x > 1200 || this.x < 0 || this.y > 1200 || this.y < 0) {
+      this.destroy();
+      console.log("dead");
+    }
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import "phaser";
+import { Title } from "./Title";
+import { MainGame } from "./MainGame";
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const config = {
+  type: Phaser.AUTO,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.FIT,
+    width: 1200,
+    height: 1200
+  },
+  backgroundColor: 0x000000,
+  scene: [Title, MainGame],
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false
+    }
+  }
+};
+
+const game = new Phaser.Game(config);
